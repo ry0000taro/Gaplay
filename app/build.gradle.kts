ce6012eb0a1b5dcf93645414ef71343931548d6b
ry0000taro <higashide.fam.r@gmail.com>
2026-03-04 16:06:55 +0900

    minSdkの引き上げ
    アプリが対応できる最小限のバージョンの引き上げを実行

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.ry0000tarodojo2026"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
