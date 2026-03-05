2df9fecf6fa6f02fc460de8a1b81a2921d32c63f
ry0000taro <higashide.fam.r@gmail.com>
2026-03-05 11:50:26 +0900

    Coilの追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.coil.compose)
     implementation(libs.converter.gson)
     implementation(libs.retrofit)
     testImplementation(libs.junit)
