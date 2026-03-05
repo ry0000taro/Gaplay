3c975a2e4157481aa69f6ff3722a64a8db3217ec
ry0000taro <higashide.fam.r@gmail.com>
2026-03-05 11:01:22 +0900

    VideoDataクラスのプロパティ変更と追加

--- app/src/main/java/com/example/ry0000tarodojo2026/data/model/VideoData.kt
+++ app/src/main/java/com/example/ry0000tarodojo2026/data/model/VideoData.kt
@@ -4,9 +4,11 @@ package com.example.ry0000tarodojo2026.data.model
  * 画面（UI）に表示するために最適化された動画データ
  */
 data class VideoData(
-    val id: Int,             // リスト内での識別用
+    val id: String,             // リスト内での識別用
     val thumbnailUrl: String, // サムネイル画像のURL
     val videoTitle: String,   // 動画のタイトル
     val channelName: String,  // チャンネル名
-    val formattedDuration: String = "" // "3:15" のように整形した再生時間（後で使います！）
+    val viewCount: String,
+    val duration: String,
+    val channelId: String
 )
