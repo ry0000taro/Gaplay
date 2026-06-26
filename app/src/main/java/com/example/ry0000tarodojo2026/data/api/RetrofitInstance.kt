22cfe09b3778d783e7e15e3aa2e4fc2ab08161e8
Ryotaro <ry0000taro.dev@gmail.com>
2026-06-26 12:58:17 +0900

    デバックログの表示条件分岐の提示

--- app/src/main/java/com/example/ry0000tarodojo2026/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/ry0000tarodojo2026/data/api/RetrofitInstance.kt
@@ -4,16 +4,18 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.example.ry0000tarodojo2026.BuildConfig
 
 object RetrofitInstance {
     private const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
     private val client: OkHttpClient by lazy {
-        val logging = HttpLoggingInterceptor()
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-
-        OkHttpClient.Builder()
-            .addInterceptor(logging)
-            .build()
+        val builder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            builder.addInterceptor(logging)
+        }
+        builder.build()
     }
 
     val api: YouTubeApiService by lazy {
