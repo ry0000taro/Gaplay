58a7f61631021bdeaf91dbbdce0a3bd80bb34c52
Ryotaro <ry0000taro.dev@gmail.com>
2026-06-27 21:21:59 +0900

    不要importの削除

--- app/src/main/java/com/example/ry0000tarodojo2026/ui/screens/TimerPlayerScreen.kt
+++ app/src/main/java/com/example/ry0000tarodojo2026/ui/screens/TimerPlayerScreen.kt
@@ -17,25 +17,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import android.content.Context
 import android.hardware.SensorManager
 import com.example.ry0000tarodojo2026.R
 import com.example.ry0000tarodojo2026.data.model.ExerciseType
 import com.example.ry0000tarodojo2026.data.model.VideoEntity
-import com.example.ry0000tarodojo2026.ui.components.VideoPlayerView
 import com.example.ry0000tarodojo2026.utils.ShakeDetector
 import java.util.Locale
 
